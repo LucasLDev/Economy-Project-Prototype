0b8b4b03e3f1972288a710ee295978b5b65fc78b
LucasLDev <liddyfelix.l@gmail.com>
2023-02-08 20:16:21 +1100

    Added safe house and small details

---
+++ EPP/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Vector3 startingPosition;
+    private Vector3 roamPosition;
+   private void Start ()
+   {
+        startingPosition = transform.position;
+        roamPosition = GetRoamingPosition();
+   }
+
+   private void Update()
+   {
+    
+   }
+
+   private Vector3 GetRoamingPosition()
+   {
+        return startingPosition + UtilsClass.GetRandomDir() * Random.Range(10f, 50f);
+   }
+}
