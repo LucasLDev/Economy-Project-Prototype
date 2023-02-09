438dce2a4c1a6289bcd2cae5fb6f2c5f0b221761
LucasLDev <liddyfelix.l@gmail.com>
2023-02-10 03:42:42 +1100

    Complete Health System

---
+++ EPP/Assets/Scripts/Player/Health.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+
+    public int maxHealth = 3;
+    public float currentHealth;
+
+    private bool dead;
+ 
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - amount, 0, maxHealth);
+
+        if(currentHealth > 0)
+        {
+            //hurt
+        }
+        else
+        {
+            //dead
+            if(!dead)
+            {   
+                //death animation
+                //anim.SetTrigger("");
+
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+                SceneManager.LoadScene(1);
+            }
+        }
+
+    }
+
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, maxHealth);
+    }
+
+    /*private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+    }*/
+}
